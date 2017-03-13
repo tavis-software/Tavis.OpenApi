5644e31b9b2525664820de8cf0dff420ea69e303
Darrel <darrel@tavis.ca>
2017-03-13 10:11:29 -0400

    sigh

--- src/OpenApi/OpenApiParser.cs
+++ src/OpenApi/OpenApiParser.cs
@@ -26,9 +26,8 @@ namespace Tavis.OpenApi
         {
             this.context = new ParsingContext(LoadReference);
             this.rootNode = new RootNode(this.context, stream);
-            return OpenApiDocument.Load(this.rootNode);
+            return OpenApiV3.LoadOpenApi(this.rootNode);
         }
-
 
         private IReference LoadReference(string pointer)
         {
@@ -49,7 +48,7 @@ namespace Tavis.OpenApi
                         referencedObject = Schema.Load(node);
                         break;
                     case "parameters":
-                        referencedObject = Parameter.Load(node);
+                        referencedObject = OpenApiV3.LoadParameter(node);
                         break;
                     case "callbacks":
                         referencedObject = Callback.Load(node);
@@ -89,65 +88,6 @@ namespace Tavis.OpenApi
             return referencedObject;
         }
 
-        public static FixedFieldMap<OpenApiDocument> OpenApiFixedFields = new FixedFieldMap<OpenApiDocument> {
-            { "openapi", (o,n) => { o.Version = n.GetScalarValue(); } },
-            { "info", (o,n) => o.Info = Info.Load(n) },
-            { "servers", (o,n) => o.Servers = n.CreateList<Server>(Server.Load) },
-            { "paths", (o,n) => o.Paths = Paths.Load(n) },
-            { "components", (o,n) => o.Components = Components.Load(n) },
-            { "tags", (o,n) => o.Tags = n.CreateList(Tag.Load)},
-            { "externalDocs", (o,n) => o.ExternalDocs = ExternalDocs.Load(n) },
-            { "security", (o,n) => o.SecurityRequirements = n.CreateList(SecurityRequirement.Load)}
-
-            };
-
-        public static PatternFieldMap<OpenApiDocument> OpenApiPatternFields = new PatternFieldMap<OpenApiDocument>
-        {
-            // We have no semantics to verify X- nodes, therefore treat them as just values.
-            { (s)=> s.StartsWith("x-"), (o,k,n)=> o.Extensions.Add(k, ((ValueNode)n).GetScalarValue()) }
-        };
-
-
-        public static FixedFieldMap<Components> ComponentsFixedFields = new FixedFieldMap<Components> {
-            { "schemas", (o,n) => { o.Schemas = n.CreateMap(Schema.Load); } },
-            { "parameters", (o,n) => o.Parameters = n.CreateMap(Parameter.Load) },
-            { "responses", (o,n) => o.Responses = n.CreateMap(Response.Load) },
-            { "responseHeaders", (o,n) => o.ResponseHeaders = n.CreateMap(Headers.Load) },
-            { "securitySchemes", (o,n) => o.SecuritySchemes = n.CreateMap(SecurityScheme.Load) },
-            { "callbacks", (o,n) => o.Callbacks = n.CreateMap(Model.Callback.Load) },
-            { "links", (o,n) => o.Links = n.CreateMap(Link.Load) },
-            };
-
-        public static PatternFieldMap<Components> ComponentsPatternFields = new PatternFieldMap<Components>
-        {
-            { (s)=> s.StartsWith("x-"), (o,k,n)=> o.Extensions.Add(k, n.GetScalarValue()) }
-        };
-
-        public static FixedFieldMap<Contact> ContactFixedFields = new FixedFieldMap<Contact> {
-            { "name", (o,n) => { o.Name = n.GetScalarValue(); } },
-            { "email", (o,n) => { o.Email = n.GetScalarValue(); } },
-            { "url", (o,n) => { o.Url = new Uri(n.GetScalarValue()); } },
-        };
-
-        public static PatternFieldMap<Contact> ContactPatternFields = new PatternFieldMap<Contact>
-        {
-            { (s)=> s.StartsWith("x-"), (o,k,n)=> o.Extensions.Add(k, n.GetScalarValue()) }
-        };
-
-        public static FixedFieldMap<Info> InfoFixedFields = new FixedFieldMap<Info>
-        {
-            { "title", (o,n) => { o.Title = n.GetScalarValue(); } },
-            { "version", (o,n) => { o.Version = n.GetScalarValue(); } },
-            { "description", (o,n) => { o.Description = n.GetScalarValue(); } },
-            { "termsOfService", (o,n) => { o.TermsOfService = n.GetScalarValue(); } },
-            { "contact", (o,n) => { o.Contact = Contact.Load(n); } },
-            { "license", (o,n) => { o.License = License.Load(n); } }
-        };
-
-        public static PatternFieldMap<Info> InfoPatternFields = new PatternFieldMap<Info>
-        {
-            { (s)=> s.StartsWith("x-"), (o,k,n)=> o.Extensions.Add(k, n.GetScalarValue()) }
-        };
 
     }
 
