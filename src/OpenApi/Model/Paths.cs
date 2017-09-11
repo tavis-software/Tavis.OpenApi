95a84d2e10215c44c44857201678f119425598bb
Darrel Miller <darrmi@microsoft.com>
2017-09-11 10:18:22 -0400

    Added AddPathItem to Paths

--- src/OpenApi/Model/Paths.cs
+++ src/OpenApi/Model/Paths.cs
@@ -8,6 +8,10 @@ namespace Tavis.OpenApi.Model
         public IDictionary<string, PathItem> PathItems { get; set; } = new Dictionary<string, PathItem>();
         public Dictionary<string, AnyNode> Extensions { get; set; } = new Dictionary<string, AnyNode>();
         
+        public void AddPathItem(string key, PathItem pathItem)
+        {
+            PathItems.Add(key, pathItem);
+        }
         public PathItem GetPath(string key)
         {
             return PathItems[key];
